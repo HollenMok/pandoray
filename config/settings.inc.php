d26eebf0b1fb5638a5a084b1eea52be823703f3f
HollenMok <461836320@qq.com>
2016-06-18 14:04:49 +0800

    pandoray = pandora yun platform
    future yun platform for pandoraf refer to prestashop

---
+++ config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'prestashop');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', '');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'UKUq2hTGpjL5oPApb7LcnyE37y5FuIT6Y4OO3X31udeooWCn6aD8N00j');
+define('_COOKIE_IV_', 'wtwf6vVq');
+define('_PS_CREATION_DATE_', '2016-06-17');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.5');
+define('_RIJNDAEL_KEY_', 'IHmnhUIooy74RmTaBpEj8kLh0SqCHf2J');
+define('_RIJNDAEL_IV_', 'glAwzgCuwNI18lzgqcRLYA==');
